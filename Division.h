fd9a18787eb3c1a489a58d0fd0fb628e17887d75
AngryVelociraptor <pauljohnecheck@gmail.com>
2016-11-03 19:41:58 -0400

    Add files via upload

--- Division.h
+++ Division.h
@@ -3,7 +3,6 @@
 #include "Player.h"
 #include <string>
 #include "role.h"
-#include "ODivision.h"
 class Division
 {
 public:
@@ -18,7 +17,6 @@ public:
 	std::string getBattletag(int index);
 	Role getRole(int index);
 	double getAverageSR();
-	ODivision teamSort(int seed);											//Sorts the players into teams. "seed" is the index of the value that the algorithm starts on. Seed MUST START ON A PLAYER WHO DID NOT CHOOSE ANY AS THIER FIRST ROLE PICK
 	//all search functions return -1 if nothing is found, as well as printing a message
 	int search(int sr);														//searches for a player with that sr
 	std::vector<int> searchAll(int sr);										//searches for all players with that sr
@@ -28,8 +26,6 @@ public:
 	int search(int lower, int upper);										//searches for a player between those bounds. Can be equal to those bounds.
 	int search(int lower, int upper, Role role);							//searches for a player with x role that >= lower and <= upper
 	std::vector<int> searchAll(int lower, int upper, Role role);			//searches for all players with x role that >= lower and <= upper
-	int findSolution(double goal, Role role);
 };
-
 
 //to add: searchAll functions with exeptions
--- Team.h
+++ Team.h
@@ -7,12 +7,8 @@ class Team
 public:
 	std::vector<Player> m_players;
 	std::string m_name;
-	int m_countTanks = 0;
-	int m_countDPS = 0;
-	int m_countSupport = 0;
 	Team()
 	{
 		m_players.resize(5);
 	}
-	double getAverage();
 };
